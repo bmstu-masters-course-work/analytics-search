83934be8b931718e63dcc9bd39df2bcecbc56352
r.abdurahmanova <roofinda@gmail.com>
2021-10-20 08:40:58 +0300

    added simple model for tile object

--- project/search/models.py
+++ project/search/models.py
@@ -1,3 +1,18 @@
+from __future__ import unicode_literals
 from django.db import models
-
-# Create your models here.
+import datetime
+ 
+class Tile(models.Model):
+ 
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+ 
+    def __unicode__(self):
+        return self.name
+ 
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+        }
